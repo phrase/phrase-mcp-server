63352737b41645e0b0298b5016b8d388aa98e06d
soren.fandrich@phrase.com <soren.fandrich@phrase.com>
2026-02-26 14:23:48 +0100

    PJM-5338: Fixing the client type to MCP

--- src/lib/runtime-info.ts
+++ src/lib/runtime-info.ts
@@ -2,4 +2,5 @@ import packageJson from "#package.json" with { type: "json" };
 
 export const APP_NAME: string = packageJson.name;
 export const APP_VERSION: string = packageJson.version;
+export const PHRASE_TMS_CLIENT_TYPE = "MCP";
 export const GLOBAL_USER_AGENT = `${APP_NAME}/${APP_VERSION}`;
